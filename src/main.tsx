2f862cecc3fc1654463f4ddee71a56146ac8e3b7
Vijesh <vijesh@Vijeshs-MacBook-Air.local>
2025-03-06 20:53:08 +0530

    added the strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,14 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './store';
 import App from './App';
 import './index.css';
+import React from 'react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
-  // </React.StrictMode>,
+  </React.StrictMode>,
 );
